25f44e86b2dea73bb1082af5474a3a38e427e709
martinmorenoc <mmoreno6@miuandes.cl>
2021-10-19 02:02:50 -0300

    implement win condition

---
+++ Assets/Scripts/Door_Controller.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Controller : MonoBehaviour
+{
+    public enum StateEnum
+    {
+        Open,
+        Close
+    }
+
+    private SpriteRenderer spriteR;
+    public Sprite doorOpen;
+    public Sprite doorClose;
+    private StateEnum doorState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteR = gameObject.GetComponent<SpriteRenderer>();
+        doorState = StateEnum.Close;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeState()
+    {
+        if (doorState == StateEnum.Close)
+        {
+            spriteR.sprite = doorOpen;
+            doorState = StateEnum.Open;
+        }
+        else if (doorState == StateEnum.Open)
+        {
+            spriteR.sprite = doorClose;
+            doorState = StateEnum.Close;
+        }   
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        //No se todavia porque no entra aca
+        if (col.gameObject.CompareTag("Player"))
+        {
+            if (doorState == StateEnum.Open)
+            {
+                Debug.Log("WIN!");
+            }
+        }
+    }
+}
--- Assets/Scripts/Game_Controller.cs
+++ Assets/Scripts/Game_Controller.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class Game_Controller : MonoBehaviour
 {
     public int torchCount;
+    private bool complete;
+    private GameObject door_Controller;
+
     // Start is called before the first frame update
     void Start()
     {
         torchCount = 0;
+        complete = false;
+        door_Controller = GameObject.FindWithTag("Door");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (torchCount == 3)
+        if ((torchCount == 2) && !complete)
         {
-            Debug.Log("Tres antorchas prendidas.");
+            door_Controller.GetComponent<Door_Controller>().ChangeState();
+            complete = true;
+        }
+        else if ((torchCount != 2) && complete)
+        {
+            door_Controller.GetComponent<Door_Controller>().ChangeState();
+            complete = false;
         }
     }
 
