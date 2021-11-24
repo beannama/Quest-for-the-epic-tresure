8082094562b68deec583fbba0f513a8a9a81a482
Benjamín Naranjo <Ben.Nar.95@gmail.com>
2021-11-24 13:55:54 -0300

    Added Fourth Lore

---
+++ Assets/Scripts/Lore/ThirdLvl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdLvl : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        //SceneManager.LoadScene("ThirdLevel", LoadSceneMode.Single);    START ThirdLevel
+    }
+}
